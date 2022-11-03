c59b51aa3f52944748763880be1fd39e44d35571
Maurice Pittet <maurice.pittet@gmail.com>
2022-11-03 15:24:56 +0100

    We are now at version 0.4.5

--- server.go
+++ server.go
@@ -25,7 +25,7 @@ import (
 )
 
 const (
-	VERSION                = "0.4.4"
+	VERSION                = "0.4.5"
 	APP                    = "go-cloud-k8s-info"
 	defaultProtocol        = "http"
 	defaultPort            = 8080
